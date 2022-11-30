ba179502dc51c36f26879a510b0a53c84734fd5e
h3rm4nn-99 <73171414+h3rm4nn-99@users.noreply.github.com>
2022-11-30 15:55:04 +0100

    Complete rewriting of the algorithms

--- code/decoding.py
+++ code/decoding.py
@@ -16,7 +16,7 @@ filepath_encoded = args.filepath_encoded
 seed = args.seed
 a_key = args.a
 
-samples_per_slice = 5000
+samples_per_frame = 5000
 
 original_track = AudioSegment.from_wav(filepath)
 encoded_track = AudioSegment.from_wav(filepath_encoded)
@@ -24,17 +24,13 @@ encoded_track = AudioSegment.from_wav(filepath_encoded)
 original_samples = original_track.get_array_of_samples()
 encoded_samples = encoded_track.get_array_of_samples()
 
-message_length = len(original_samples) / samples_per_slice # lunghezza massima
-
-r_sequence = utils.gen_r_sequence(message_length, seed, a_key)
-
-message = ""
-for i in range (0, len(original_samples), samples_per_slice):
-    if original_samples[i] != encoded_samples[i]:
-        if r_sequence[i]:
-            message += "1"
-        else:
-            message += "0"
-
-print("Messaggio decodificato: " + message)
-sys.exit(0)
+count = 0
+leng = 0
+for i in range(0, len(original_samples), samples_per_frame):
+    if (leng <= 32):
+        if original_samples[i] != encoded_samples[i]:
+            print("originale " + str(i) + " value: " + str(original_samples[i]))
+            print("modificato " + str(i) + " value: " + str(encoded_samples[i]))
+            count += 1
+    leng += 1
+print(count)
--- code/encoding.py
+++ code/encoding.py
@@ -1,10 +1,9 @@
 from pydub import AudioSegment
 from bitarray import bitarray
-import utils as utils
 import sys
-import fileinput
 import argparse
 import os
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--message', type=str, required=True)
@@ -13,60 +12,63 @@ parser.add_argument('--seed', type=int, required=True)
 parser.add_argument('--a', type=int, required=True)
 args = parser.parse_args()
 
-msg = args.message
+current_number = args.seed
+a_key = int(args.a) 
 filepath = args.filepath
-seed = args.seed
-a_key = args.a
+message = args.message
 
-echo_loudness = -10 #dB
-echo_delay = 1 #50ms
-samples_per_slice = 5000
+samples_per_frame = 5000
+
+message_size = len(message)
 
 message_bitarray = bitarray()
-message_bitarray.frombytes(msg.encode('utf-8'))
-message_length = len(message_bitarray)
+message_bitarray.frombytes(message.encode('utf-8'))
+frame_da_modificare = len(message_bitarray)
+R_sequence = bitarray(frame_da_modificare)
 
-r_sequence = utils.gen_r_sequence(message_length, seed, a_key)
+for i in range(0, frame_da_modificare):
+    current_number = (a_key * current_number + 2*a_key) % frame_da_modificare
 
-original_track = AudioSegment.from_mp3(filepath)
-echoed_track = AudioSegment.empty()
+    if current_number % 6 > 2:
+        R_sequence[i] = True
+    else:
+        R_sequence[i] = False
+print("ERRE " + str(len(R_sequence)))
+echoed_song = AudioSegment.empty()
+original_song = AudioSegment.from_wav(filepath)
 
-number_of_frames = int(original_track.frame_count())
-available_bits= number_of_frames / samples_per_slice
+loudness = -10
+delay = 1
 
-if available_bits < message_length:
-    print("Non ci sono abbastanza bit disponiili per nascondere un messaggio di lunghezza " + str(message_length))
+original_song_frame_number = int(original_song.frame_count())
+available_bits = original_song_frame_number / samples_per_frame
+
+if (available_bits < frame_da_modificare):
+    print("Non sono disponibili abbastanza frame per nascondere il messaggio")
     sys.exit(0)
 
 count = 0
-for i in range (0, message_length):
-    current_slice_start = i * samples_per_slice
-    current_slice_end = (i + 1) * samples_per_slice
-    count += samples_per_slice
+for i in range(0, frame_da_modificare):
+    start_index = i * samples_per_frame
+    end_index = (i+1) * samples_per_frame
+    count += samples_per_frame
 
-    slice_to_process = original_track.get_sample_slice(current_slice_start, current_slice_end)
-
-    processed_slice = None
-    if r_sequence[i]:
-        if message_bitarray[i]:
-            processed_slice = slice_to_process.get_array_of_samples()
+    porzione_eco = original_song.get_sample_slice(start_index, end_index)
+    if i < frame_da_modificare:
+        if R_sequence[i]:
+            if message_bitarray[i]:
+                echoed = porzione_eco.get_array_of_samples()
+            else:
+                echoed = porzione_eco.overlay(porzione_eco.apply_gain(loudness), position=delay)
+                echoed = echoed.get_array_of_samples()
         else:
-            processed_slice = slice_to_process.overlay(slice_to_process.apply_gain(echo_loudness), position=echo_delay)
-            processed_slice = processed_slice.get_array_of_samples()
-    else:
-        if message_bitarray[i]:
-            processed_slice = slice_to_process.overlay(slice_to_process.apply_gain(echo_loudness), position=echo_delay)
-            processed_slice = processed_slice.get_array_of_samples()
-        else:
-            processed_slice = slice_to_process.get_array_of_samples()
-    
-    echoed_track = echoed_track + original_track._spawn(processed_slice)
+            if message_bitarray[i]:
+                echoed = porzione_eco.overlay(porzione_eco.apply_gain(loudness), position=delay)
+                echoed = echoed.get_array_of_samples()
+            else:
+                echoed = porzione_eco.get_array_of_samples()
+        echoed_song = echoed_song + original_song._spawn(echoed)
 
-# i samples residui vengono lasciati intatti e ricopiati nella traccia audio modificata
+echoed_song = echoed_song + original_song.get_sample_slice(count, original_song_frame_number)
 
-echoed_track = echoed_track + original_track.get_sample_slice(count, number_of_frames)
-if not os.path.exists('./output'):
-    os.mkdir('./output')
-echoed_track.export("./output/echoed_track.wav", format='wav')
-
-sys.exit(0)
+echoed_song.export('echoed_song.wav', format='wav')
